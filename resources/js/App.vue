25e187f18dd6f77d304d54d47147767e293b9324
Jumanji81 <deenl@gmx.com>
2020-02-21 00:54:52 +0100

    Setup

---
+++ resources/js/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-app :style="'background: #f5f5f5'">
+        <!-- class="grey lighten-4 -->
+        <Navbar />
+        <v-content class="mx-4 mb-4">
+            <router-view></router-view>
+        </v-content>
+    </v-app>
+  </template>
+
+<script>
+import Navbar from "./components/Navbar";
+
+export default {
+  // r: export name?
+  // name: "App",
+  components: { Navbar },
+  data: () => ({
+      //
+    }),
+};
+</script>
---
+++ resources/js/app.js
@@ -0,0 +1,30 @@
+// require lodash, axios and csrf-token
+// r: extensions with require/import
+require('./plugins/bootstrap.js');
+
+// q: 266kb , bit much ?
+// import '@mdi/font/css/materialdesignicons.css'
+
+// q: need to register Vue globally (window.)?
+// import Vue from 'vue'
+window.Vue = require('vue');
+import vuetify from "./plugins/vuetify.js"
+import router from './plugins/router.js'
+
+Vue.config.productionTip = false
+
+// q: difference from importing @ app.js? 
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+import App from "./App.vue"
+
+// fresh Vue instance injected into index laravel view #root element 
+// r/q: link to constant variable?
+const app = new Vue({
+    router,
+    vuetify,
+    // r/q: this or render difference?
+    el: "#root",
+    components: {App}
+    //   render: h => h(App)
+});
+// }).$mount('#root');

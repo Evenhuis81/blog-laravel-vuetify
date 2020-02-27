15d9188b3334a0f4df2f597dc658b8b1dc045c53
Evenhuis81 <devenhuis81@gmail.com>
2020-02-27 10:50:34 +0100

    Update Ignore

--- resources/js/views/Index.vue
+++ resources/js/views/Index.vue
@@ -1,6 +1,6 @@
   <!-- <v-card class="mx-12" max-width="1500"> -->
   <template>
-  <v-card class="ma-10">
+  <v-card class="ma-12">
     <v-carousel cycle :show-arrows="false">
       <v-carousel-item
         v-for="(item,i) in items"
@@ -25,42 +25,42 @@ export default {
     return {
       items: [
         {
-          src: "/storage/categoryimages/Movies - video-videoformats-2340x700.jpg"
-        },
-                {
-          src: "/storage/categoryimages/Upcoming 2020 - online-marketing-trends-2020-2340x700.jpg"
-        },
-                {
-          src: "/storage/categoryimages/Music - meisje-luistert-naar-muziek-op-koptelefoon-2340x700.jpg"
-        },
-                {
-          src: "/storage/categoryimages/Dating - klantcontact-menscontact-2340x700.jpg"
-        },
-                {
-          src: "/storage/categoryimages/Mobile Trends - conversie-optimalisatie-CRO-trends-2340x700.jpg"
-        },
-                {
-          src: "/storage/categoryimages/New Inventions - lamp-blog-1-2340x700.jpg"
-        },
-                {
-          src: "/storage/categoryimages/Social Media - Facebook-1-2340x700.jpg"
-        },
-        {
-          src: "./storage/categoryimages/Adventure - customer-journey-roller-coaster-2340x700.jpg"
-        },
-        {
-          src: "./storage/categoryimages/Art - Linkbuilding-2340x700.jpg"
-        },
-        {
-          src: "./storage/categoryimages/Classic Stuff - customer-2340x700.jpg"
+          src: "/storage/categoryimages/Movies-2340x700.jpg"
         }
+        //         {
+        //   src: "/storage/categoryimages/Upcoming 2020 - online-marketing-trends-2020-2340x700.jpg"
+        // },
+        //         {
+        //   src: "/storage/categoryimages/Music - meisje-luistert-naar-muziek-op-koptelefoon-2340x700.jpg"
+        // },
+        //         {
+        //   src: "/storage/categoryimages/Dating - klantcontact-menscontact-2340x700.jpg"
+        // },
+        //         {
+        //   src: "/storage/categoryimages/Mobile Trends - conversie-optimalisatie-CRO-trends-2340x700.jpg"
+        // },
+        //         {
+        //   src: "/storage/categoryimages/New Inventions - lamp-blog-1-2340x700.jpg"
+        // },
+        //         {
+        //   src: "/storage/categoryimages/Social Media - Facebook-1-2340x700.jpg"
+        // },
+        // {
+        //   src: "./storage/categoryimages/Adventure - customer-journey-roller-coaster-2340x700.jpg"
+        // },
+        // {
+        //   src: "./storage/categoryimages/Art - Linkbuilding-2340x700.jpg"
+        // },
+        // {
+        //   src: "./storage/categoryimages/Classic Stuff - customer-2340x700.jpg"
+        // }
       ]
     };
   },
   name: "Blogs",
   mounted() {
     this.$store.dispatch("fetchBlogs");
-    this.$store.dispatch("fetchCategories")
+    this.$store.dispatch("fetchCategories");
   },
   methods: {
     deleteBlog(blog) {
